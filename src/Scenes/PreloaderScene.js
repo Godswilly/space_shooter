c5cae4139d6ec229b93c3e938f42dbbb24bbb7fe
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-04-02 14:25:56 +0100

    Update Scenes

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -21,16 +21,16 @@ export default class PreloaderScene extends Phaser.Scene {
     progressBox.fillStyle(0x222222, 0.8);
     progressBox.fillRect(240, 270, 320, 50);
 
-    const { width } = this.cameras.main.width;
-    const { height } = this.cameras.main.height;
+    const width = this.cameras.main.width;
+    const height = this.cameras.main.height;
     const loadingText = this.make.text({
       x: width / 2,
       y: height / 2 - 50,
       text: 'Loading...',
       style: {
         font: '20px monospace',
-        fill: '#ffffff',
-      },
+        fill: '#ffffff'
+      }
     });
     loadingText.setOrigin(0.5, 0.5);
 
@@ -40,8 +40,8 @@ export default class PreloaderScene extends Phaser.Scene {
       text: '0%',
       style: {
         font: '18px monospace',
-        fill: '#ffffff',
-      },
+        fill: '#ffffff'
+      }
     });
     percentText.setOrigin(0.5, 0.5);
 
@@ -51,14 +51,14 @@ export default class PreloaderScene extends Phaser.Scene {
       text: '',
       style: {
         font: '18px monospace',
-        fill: '#ffffff',
-      },
+        fill: '#ffffff'
+      }
     });
     assetText.setOrigin(0.5, 0.5);
 
     // update progress bar
     this.load.on('progress', (value) => {
-      percentText.setText(`${parseInt(value * 100)}%`);
+      percentText.setText(parseInt(value * 100) + '%');
       progressBar.clear();
       progressBar.fillStyle(0xffffff, 1);
       progressBar.fillRect(250, 280, 300 * value, 30);
@@ -66,7 +66,7 @@ export default class PreloaderScene extends Phaser.Scene {
 
     // update file progress text
     this.load.on('fileprogress', (file) => {
-      assetText.setText(`Loading asset: ${file.key}`);
+      assetText.setText('Loading asset: ' + file.key);
     });
 
     // remove progress bar when complete
@@ -81,16 +81,22 @@ export default class PreloaderScene extends Phaser.Scene {
 
     this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
 
-    // load assets needed in our game
-    this.load.image('blueButton1', 'assets/ui/blue_button02.png');
-    this.load.image('blueButton2', 'assets/ui/blue_button03.png');
-    this.load.image('phaserLogo', 'assets/image/logo.png');
+  // load assets needed in our game
+
+  this.load.image('blueButton1', 'assets/ui/blue_button02.png');
+  this.load.image('blueButton2', 'assets/ui/blue_button03.png');
+  this.load.image('phaserLogo', 'assets/logo.png');
+  this.load.image('box', 'assets/ui/grey_box.png');
+  this.load.image('checkedBox', 'assets/ui/blue_boxCheckmark.png');
+  this.load.audio('bgMusic', ['assets/TownTheme.mp3']);
   }
 
-  ready() {
-    this.readyCount++;
-    if (this.readyCount === 2) {
-      this.scene.start('Title');
-    }
+
+
+ready () {
+  this.scene.start('Title')
+  this.readyCount++;
+  if (this.readyCount === 2) {
+    this.scene.start('Title');
   }
-}
+} 
